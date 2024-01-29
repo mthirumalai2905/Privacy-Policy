6a12422dcb710b4d112619ccd8e533339e819186
Thirumalai <98790479+mthirumalai2905@users.noreply.github.com>
2024-01-29 12:15:35 +0530

    g

--- src/Home.jsx
+++ src/Home.jsx
@@ -5,8 +5,8 @@ const Home = () => {
   return (
     <div>
       <div className='home-page'>
-      <h1>Enter service in the link to go to terms and services</h1>
-      <h1>Enter policy in the link to go to terms and policies</h1>
+      <h1>Enter service in the link to go to terms and services like https://www.example.com/terms/</h1>
+      <h1>Enter policy in the link to go to terms and policies like https://www.example.com/policy/</h1>
       </div>
     </div>
   )
